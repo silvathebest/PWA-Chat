6d45464863fdb3fd0ff648468566096a67e77b44
Alexey Romanov <51180322+silvathebest@users.noreply.github.com>
2021-04-15 01:38:07 +0400

    fix encrypt.js

--- client/src/encryption/encrypt.js
+++ client/src/encryption/encrypt.js
@@ -1,16 +1,24 @@
 const encryption = (text, key) => {
     let textArr = Array.from(text)
-    for (let i = 0, j = 0; i < text.length; i++, j++) {
-        j = (i === key.length - 1) ? 0 : j;
+    let multiple = 1
+    for (let i = 0, j = 0; i < textArr.length; i++, j++) {
+        if(i === (key.length - 1) * multiple){
+            j = 0;
+            multiple++;
+        }
         textArr[i] = String.fromCharCode(key.charCodeAt(j) + text.charCodeAt(i))
     }
     return textArr.join('')
 }
 
 const decryption = (text, key) => {
-    let textArr = Array.from(text)
-    for (let i = 0, j = 0; i < text.length; i++, j++) {
-        j = (i === key.length - 1) ? 0 : j
+    const textArr = Array.from(text)
+    let multiple = 1
+    for (let i = 0, j = 0; i < textArr.length; i++, j++) {
+        if(i === (key.length - 1) * multiple){
+            j = 0;
+            multiple++;
+        }
         textArr[i] = String.fromCharCode(text.charCodeAt(i) - key.charCodeAt(j))
     }
     return textArr.join('')
